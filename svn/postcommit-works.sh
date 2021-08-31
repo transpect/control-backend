70f2cd5b1f434f41e165e60065ade2a26c5353b9
Gerrit Imsieke <gerrit.imsieke@le-tex.de>
2021-08-31 12:51:56 +0200

    more configurable

--- svn/postcommit-works.sh
+++ svn/postcommit-works.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# replace it for installation, for example:
+# sed -e 's@/home/transpect-control@'"$HOME"'@' $1/backend/svn/postcommit-works.sh
 source /home/transpect-control/envvars.sh
 
-LOGDIR=/data/svn/tmp/postcommit/$1
+if [ -z $LOGDIRBASE ]; then
+    LOGDIRBASE=/data/svn/tmp/postcommit
+fi
+if [ -z $BASEXBASEURL ]; then
+    BASEXBASEURL=http://localhost:$BASEXHTTPPORT
+fi
+
+LOGDIR=$LOGDIRBASE$1
 LOGFILE=$LOGDIR/$2.log
 ERRORLOGFILE=$LOGDIR/$2.errors
 
@@ -11,6 +20,4 @@ mkdir -p $LOGDIR
 echo $1 $2 > $LOGFILE
 svnlook changed -r$2 $1 >> $LOGFILE 2>> $ERRORLOGFILE
 
-echo BASEXHTTPPORT=$BASEXHTTPPORT
-
-curl -X POST --data-binary @$LOGFILE http://localhost:$BASEXHTTPPORT/control-backend/default/process-commit-log 2>&1 >> $ERRORLOGFILE
+curl -X POST --data-binary @$LOGFILE $BASEXBASEURL/control-backend/default/process-commit-log 2>&1 >> $ERRORLOGFILE
